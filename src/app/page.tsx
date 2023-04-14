ff4e72f561f1703e656a9cc59929ce7a211bc4bd
Abraham Duno <abraham.d@servicepad.com>
2023-04-14 11:22:23 -0400

    re init

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import { TICKER_SYMBOLS, Ticker, getTicker } from '@/../script/state/repository/ticker'
+import TickerCard from '@/dom/atom/TickerCard'
+import Scene from '@/model/core/Scene';
+
+export default async function Home() {
+
+  const tickers: Ticker[] = await Promise.all([ getTicker(), getTicker("USDCUSDT") ]);
+  
+  const tickerCards = TICKER_SYMBOLS.map((tickerName:any, index:number) => (
+    <TickerCard initialTicker={tickers[index]} tickerName={tickerName} key={tickerName} />
+  )); 
+
+  return (
+    <main style={{minHeight:"100vh",textAlign:"center"}}>
+      <br /> <div> {tickerCards} </div> <br />
+      <h1 style={{zIndex:"300",position: "relative"}}> <a href="/dashboard"> Dashboard </a> </h1>
+
+      <div style={{position:"absolute", height:"100%",width:"100%",top:"0"}}>
+        <Scene />
+      </div>
+    </main>
+  )
+}
